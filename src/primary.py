a7f024fdbef7a900ee6ccf88f20b121c87debf5a
greninja <sshadab84@gmail.com>
2018-02-07 02:55:31 +0530

    * Adding code for generating satisfying models for inputs evaluating to both 1 and 0 * Generating large number of models - Need to look into that!

--- src/primary.py
+++ src/primary.py
@@ -1,46 +1,23 @@
 import numpy as np 
 from z3 import *
+import time
 
-#Creating synthesized dataset
-inputs, outputs = [np.random.randint(0,2,6) for _ in xrange(5)], [np.random.randint(0,2,1) for _ in xrange(5)]
-trueoutputs, falseoutputs = [i for i, x in enumerate(outputs) if x==1], \
-                                                    [i for i, x in enumerate(outputs) if x==0]
+#Synthesizing the dataset
+inputs = [np.random.randint(0,2,6) for _ in xrange(5)]
+outputs = [np.random.randint(0,2,1) for _ in xrange(5)]
+trueoutputs = [i for i, x in enumerate(outputs) if x==1]                                                    
+falseoutputs = [i for i, x in enumerate(outputs) if x==0]
 on_indices = map(lambda index : np.where(inputs[index] == 1)[0], xrange(len(inputs)))
 off_indices = map(lambda index : np.where(inputs[index] == 0)[0], xrange(len(inputs)))
 
-P_ON_MATRIX = []
-for clause in range(1,3): 
-    temp = []
-    for index in trueoutputs:
-        parr = []
-        for elem in on_indices[index]:
-            pi = Bool('p'+str(elem)+str(clause))
-            parr.append(pi)
-        temp.append(parr)
-    P_ON_MATRIX.append(temp)
-
-N_OFF_MATRIX = []
-for clause in range(1,3): 
-    temp = []
-    for index in trueoutputs:
-        narr = []
-        for elem in off_indices[index]:
-            ni = Bool('n'+str(elem)+str(clause))
-            narr.append(ni)
-        temp.append(narr)
-    N_OFF_MATRIX.append(temp)  
-
-#Generating all-SAT only for '1' minterms:
-matrix = [] # Would contain all the clauses generated from minterms which are 1
-for i in xrange(len(P_ON_MATRIX)):
-    for j in xrange(len(P_ON_MATRIX[0])):
-        matrix.append(Or(pmatrix[i][j] + nmatrix[i][j]))
-models = get_models(matrix)
-
-# All-SAT procedure in Z3Py
-x, y, z = Bools('x y z') 
-# Return all the satisfying models of the clauses given in 'F' 
 def get_models(F):
+    """
+    Return all the satisfying models of the clauses given in 'F'.
+    This piece of code exists primarily because Z3Py does not have 
+    inbuilt support for returning all the satisfying models.
+    Hence we explicitly put a constraint to not generate a model
+    similar to previous ones.
+    """
     result = []
     s = Solver()
     s.add(F)
@@ -61,12 +38,75 @@ def get_models(F):
         s.add(Or(block))
     return result
 
-# 4.2.18 - Getting values of variables of a BoolVector
+# Producing the contraint clauses for minterms at which the function evaluates to 1
+ON_ON_MATRIX = []
+for clause in range(1,3): 
+    temp = []
+    for index in trueoutputs:
+        arr = []
+        for elem in on_indices[index]:
+            pi = Bool('p'+str(elem)+str(clause))
+            arr.append(pi)
+        temp.append(arr)
+    ON_ON_MATRIX.append(temp)
+
+OFF_OFF_MATRIX = []
+for clause in range(1,3): 
+    temp = []
+    for index in trueoutputs:
+        arr = []
+        for elem in off_indices[index]:
+            ni = Bool('n'+str(elem)+str(clause))
+            arr.append(ni)
+        temp.append(arr)
+    OFF_OFF_MATRIX.append(temp)  
+
+# Combining the clauses from the individual matrices
+matrix = [] 
+for i in xrange(len(ON_ON_MATRIX)):
+    for j in xrange(len(ON_ON_MATRIX[0])):
+        matrix.append(Or(ON_ON_MATRIX[i][j] + OFF_OFF_MATRIX[i][j]))
+
+# Producing the contraint clauses for minterms at which the function evaluates to 0
+ON_OFF_MATRIX = []
+for clause in range(1, 3):          
+    temp = []                              
+    for index in falseoutputs:                                      
+       arr = []
+       for elem in on_indices[index]: 
+           ni = Bool('n'+str(elem)+str(clause))
+           arr.append(ni)
+       temp.append(arr)
+    ON_OFF_MATRIX.append(temp)
+
+OFF_ON_MATRIX = []
+for clause in range(1,3):           
+    temp = []                              
+    for index in falseoutputs:                                      
+        arr = []
+        for elem in off_indices[index]:
+            pi = Bool('p'+str(elem)+str(clause))
+            arr.append(pi)
+        temp.append(arr)
+    OFF_ON_MATRIX.append(temp)
+
+for i in xrange(len(ON_OFF_MATRIX)):
+    for j in xrange(len(ON_OFF_MATRIX[0])):
+        matrix.append(Or(ON_OFF_MATRIX[i][j] + OFF_ON_MATRIX[i][j]))
+
+#Generating all the satisfying assignments
+start_time = time.time()
+models = get_models(matrix)
+end_time = time.time()
+
+# Retrieving the values of variables from the models returned
 vec = BoolVector('n', 10)
 G = [Or(vec)]
-models = get_models(G, 22) # since it returns exactly 22 models
+models = get_models(G) 
 for solution in models:
     length = len(solution)
     for j in range(length):
         print solution[solution[j]]
     print "\n"
+# For variables that are not present in the satisfying models implying they could take either of the values,
+# do not consider them in final solution because their values wont really matter
