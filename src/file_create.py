73eca2339d8ca78ca57dd00f069aa74e0634b476
greninja <sshadab84@gmail.com>
2018-02-21 12:18:20 +0530

    1) Adding functionality for parsing command-line argument for 'file_generate.py' 2) Generating only a single satisfying model instead of ALl-SAT 3) Resolving inconsistency issues with OFF-SET samples 4) Adding code for cross checking the generated function with input-output samples

--- src/file_create.py
+++ src/file_create.py
@@ -1,8 +1,17 @@
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("samplesize", type=int,
+	   help="Number of input samples")
+parser.add_argument("bitveclen", type=int,
+	   help="Length of the input bit vectors")
+
+args = parser.parse_args()
 with open("input.txt","w+") as f:                                                 
-  for _ in xrange(5):
-     for _ in xrange(6):
-         f.write(str(np.random.randint(0,2)))
-     f.write(" ")
-     f.write(str(np.random.randint(0,2)))
-     f.write("\n")  
+  for _ in xrange(args.samplesize):
+    for _ in xrange(args.bitveclen):
+        f.write(str(np.random.randint(0,2)))
+    f.write(" ")
+    f.write(str(np.random.randint(0,2)))
+    f.write("\n")  
--- src/primary.py
+++ src/primary.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import numpy as np 
 from z3 import *
 from datetime import datetime
@@ -20,6 +21,116 @@ on_indices = map(lambda index : np.where(np.array(inputs[index]) == 1)[0], \
                                                              xrange(len(inputs)))
 off_indices = map(lambda index : np.where(np.array(inputs[index]) == 0)[0], \
                                                              xrange(len(inputs)))
+
+s=Solver()
+
+# Generating the constraint clauses
+# Constraint 1:
+for index in trueoutputs:   
+    for clause in xrange(1,3):
+        clause_array = []
+        for elem in on_indices[index]:
+            pi = Bool('p'+str(elem)+str(clause))
+            clause_array.append(pi)
+        for elem in off_indices[index]:
+            ni = Bool('n'+str(elem)+str(clause))
+            clause_array.append(ni)
+        s.append(Or(clause_array))
+
+# Constraint 2:
+d = dict()
+for index in falseoutputs:
+    arr = []
+    for clause_indice in xrange(1, 3):
+        z = 'z'+str(index)+str(clause_indice)
+        d[z] = Bool(z)
+        arr.append(Not(d[z])) 
+    s.append(Or(arr))    
+
+# Constraint 3:
+for index in falseoutputs:
+    for clause in xrange(1, 3):   
+        arr = []          
+        for elem in on_indices[index]:                                     
+            pi = Bool('p'+str(elem)+str(clause))          
+            arr.append(Or(d['z'+str(index)+str(clause)], Not(pi)))     
+        for elem in off_indices[index]:                        
+            ni = Bool('n'+str(elem)+str(clause))                           
+            arr.append(Or(d['z'+str(index)+str(clause)], Not(ni)))
+        s.add(And(arr)) 
+
+# Constraint 4:
+# Additional constraints for restricting 2 polarities of the same variable  
+# to be not present in the same clause
+parray, narray = [], []
+for i in xrange(len(inputs)+1):
+    for j in xrange(1, 3):
+        pi, ni = Not(Bool('p'+str(i)+str(j))), Not(Bool('n'+str(i)+str(j)))
+        parray.append(pi); narray.append(ni)
+zipped = zip(parray, narray)
+for c in zipped:
+    s.append(Or(c))
+
+# Constraint 5:
+# Following piece of code creates:
+# (1) additional constraints so that each clause in the function has atmost
+#     two 'True' assignments
+# (2) a dictionary for mapping encoding variables to function variables
+dictionary = dict()
+for clause_indice in xrange(1, 3):
+    clause_array = []
+    for var_indice in xrange(6):
+        pi, ni = 'p'+str(var_indice)+str(clause_indice), \
+                                    'n'+str(var_indice)+str(clause_indice)
+        dictionary[pi], dictionary[ni] = 'x'+str(var_indice), \
+                                                'Not(x'+str(var_indice)+')'
+        clause_array.append(Bool(pi)) 
+        clause_array.append(Bool(ni))
+    s.append(Sum([If(x, 1, 0) for x in clause_array]) <= 2)
+
+s.check()
+sample = s.model()
+
+# Constructing the possible function 
+c1, c2 = [], []
+for e in sample:
+    if sample[e] == True and not str(e).startswith('z'):
+        if str(e).endswith('1'):
+            c1.append(dictionary[str(e)])
+        else:
+            c2.append(dictionary[str(e)])   
+string = ''
+if len(c1) == 2:
+    string += '('+c1[0]+' or '+c1[1]+') AND '
+elif len(c1) == 1:
+    string += '('+c1[0]+')' + ' or '
+else:
+    pass
+    
+if len(c2) == 2:
+    string += '('+c2[0]+' or '+c2[1]+')'
+elif len(c2) == 1:
+    string += '('+c2[0]+')'
+else:
+    pass
+pprint.pprint(string)
+
+# Returns the boolean value of an input variable
+def boolval(inp, el):                
+    if el.startswith('N'):                                                                                                                  
+        return np.logical_not(inp[int(el[-2])])
+    else:                       
+        return inp[int(el[-1])]
+
+# Cross checking 
+for inp, out in zip(inputs, outputs):
+    a = 0
+    for s in c1:
+        a = a or boolval(inp, s)
+    b = 0
+    for s in c2:
+        b = b or boolval(inp, s)
+    print np.logical_and(a, b)
 
 def get_models(F):
     """
@@ -48,106 +159,3 @@ def get_models(F):
             block.append(c != m[d])
         s.add(Or(block))
     return result
-
-# Generating the constraint clauses
-# Constraint 1:
-clauses = []
-for index in trueoutputs:   
-    for clause in xrange(1,3):
-        clause_array = []
-        for elem in on_indices[index]:
-            pi = Bool('p'+str(elem)+str(clause))
-            clause_array.append(pi)
-        for elem in off_indices[index]:
-            ni = Bool('n'+str(elem)+str(clause))
-            clause_array.append(ni)
-        clauses.append(Or(clause_array))
-
-# Constraint 2:
-# Either of the CNF can be zero for the overall value to evaluate to zero
-z1, z2 = Bools('z1 z2')
-d = dict()
-for clause_indice in xrange(1, 3):
-    d['z'+str(clause_indice)] = Bool('z'+str(clause_indice))
-clauses.append(Or(Not(z1), Not(z2))) 
-
-# Constraint 3:
-for index in falseoutputs:
-    for clause in xrange(1,3):
-        arr1, arr2 = [], []
-        for elem in on_indices[index]:
-            pi, ni = Bool('p'+str(elem)+str(clause)), Bool('n'+str(elem)+str(clause))
-            arr1.append(ni); arr2.append(pi)
-        for elem in off_indices[index]:
-            pi, ni = Bool('p'+str(elem)+str(clause)), Bool('n'+str(elem)+str(clause))
-            arr1.append(pi)
-            arr2.append(ni)
-        clauses.append(Or(And(Not(d['z'+str(clause)]), \
-                                Or(arr1)), And(d['z'+str(clause)], Or(arr2))))
-
-# Constraint 4:
-# Additional constraints for restricting 2 polarities of the same variable  
-# to be not present in the same clause
-parray, narray = [], []
-for i in xrange(len(inputs)+1):
-    for j in xrange(1, 3):
-        pi, ni = Not(Bool('p'+str(i)+str(j))), Not(Bool('n'+str(i)+str(j)))
-        parray.append(pi); narray.append(ni)
-zipped = zip(parray, narray)
-for c in zipped:
-    clauses.append(Or(c))
-
-# Constraint 5:
-# Following piece of code creates:
-# (1) additional constraints so that each clause in the function has atmost
-#     two 'True' assignments
-# (2) a dictionary for mapping encoding variables to function variables
-dictionary = dict()
-for clause_indice in xrange(1, 3):
-    clause_array = []
-    for var_indice in xrange(6):
-        pi, ni = 'p'+str(var_indice)+str(clause_indice), \
-                                    'n'+str(var_indice)+str(clause_indice)
-        dictionary[pi], dictionary[ni] = 'x'+str(var_indice), \
-                                                'Not(x'+str(var_indice)+')'
-        clause_array.append(Bool(pi)) 
-        clause_array.append(Bool(ni))
-    clauses.append(Sum([If(x, 1, 0) for x in clause_array]) <= 2)
-
-# Generating all the satisfying assignments to the constraint clauses
-start_time = datetime.now()   
-models = get_models(clauses)
-end_time = datetime.now()
-total_time = (end_time - start_time).total_seconds()
-print "Time taken to generate the models : {} \n  \
-        Number of models generated : {}".format(total_time, len(models))
-
-# Constructing the possible functions (as of now : just 5 random functions)
-samples, functions = [], []
-for rand in np.random.choice(len(models), 5, replace=False):
-    samples.append(models[rand])
-for sample in samples:
-    c1, c2 = [], []
-    for e in sample:
-        if sample[e] == True and not str(e).startswith('z'):
-            if str(e).endswith('1'):
-                c1.append(dictionary[str(e)])
-            else:
-                c2.append(dictionary[str(e)])   
-    string = ''
-    if len(c1) == 2:
-        string += c1[0]+' or '+c1[1]+' AND '
-    elif len(c1) == 1:
-        string += c1[0] + ' AND '
-    else:
-        pass
-        
-    if len(c2) == 2:
-        string += c2[0]+' or ' +c2[1]
-    elif len(c2) == 1:
-        string += c2[0]
-    else:
-        pass
-    functions.append(string)
-
-pprint.pprint(functions)
